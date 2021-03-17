5285ca8c693204ca527779976dae8e886f1b75c4
Vitor <Vitor@DESKTOP-IBVV4TJ>
2021-03-16 21:22:28 -0300

    Exercicio sistema de Vendas de ingressos

--- src/controller/SistemaCompras.java
+++ src/controller/SistemaCompras.java
@@ -7,7 +7,6 @@ public class SistemaCompras extends Thread{
 	private int idThread;
 	private boolean validacao;
 	private int totalIngressos = 100;
-	private int restaIngressos;
 	private Semaphore semaforo;
 	
 	public SistemaCompras(int i, Semaphore semaforo) {
@@ -39,9 +38,9 @@ public class SistemaCompras extends Thread{
 		
 	private void validacaoCompra() {
 		int qtdComprada = new Random().nextInt(5);
-		restaIngressos = totalIngressos - qtdComprada;
-		if (restaIngressos >= 0) {
-		System.out.println("#"+idThread+":  comprou "+qtdComprada+" ingressos e sobraram"+restaIngressos +" ingressos." );
+		totalIngressos -=  qtdComprada;
+		if (totalIngressos >= 0) {
+		System.out.println("#"+idThread+":  comprou "+qtdComprada+" ingressos e sobraram"+ totalIngressos +" ingressos." );
 	}
 		else {
 		System.out.println("Não tinha ingressos suficientes para o que a #"+idThread+" queria, então não fez a compra");
@@ -55,11 +54,11 @@ public class SistemaCompras extends Thread{
 		double tempoCompra = Math.random() * (3 - 1) + 1;
 			if (tempoCompra > 2.5) {
 				validacao = false;
-				System.out.println("#"+idThread+" : timeout: ultrapassou o tempo de seção, não poderá realizar a compra");
+				System.out.println("#"+idThread+" : timeout - ultrapassou o tempo de seção, não poderá realizar a compra");
 			}
 			else{
 				validacao = true;
-				System.out.println("Tempo de compra de "+tempoCompra+" segundos");
+				System.out.println("Processo de compra : "+tempoCompra+" segundos");
 			}
 			try {
 				sleep((long) tempoCompra);
@@ -72,9 +71,11 @@ public class SistemaCompras extends Thread{
 	private void loginSistema() {
 		double tempoLogin = Math.random() * (2 - 0.05) + 0.05;
 		if (tempoLogin > 1) {
+			validacao = false;
 			System.out.println("tempo de login excedido, não poderá fazer a compra");
 		}
 		else {
+			validacao = true;
 			System.out.println("Tempo de login de " +tempoLogin+" segundos.");
 		}
 			try {
