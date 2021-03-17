bb32cbe4ce670c5c89173fff6d9d2e5948cedeb2
Vitor <Vitor@DESKTOP-IBVV4TJ>
2021-03-16 21:15:45 -0300

    Exercicio Sistema de venda de ingressos

---
+++ src/view/PrincipalIngressos.java
@@ -0,0 +1,21 @@
+package view;
+
+import java.util.concurrent.Semaphore;
+
+import controller.SistemaCompras;
+
+public class PrincipalIngressos {
+
+	public static void main(String[] args) {
+		int permissoes = 1;
+		Semaphore semaforo  = new Semaphore(permissoes);
+		
+		for (int i = 0; i <= 300; i++) {
+			Thread tSisCompra = new SistemaCompras(i, semaforo);
+			tSisCompra.start();
+
+		}
+	}
+}
+
+
